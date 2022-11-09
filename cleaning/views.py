d994e7de7e28204cb5022094b32bd2ad1935f646
brian-hornbrook <brian_hornbrook@outlook.com>
2022-11-09 13:57:18 -0600

    added showing average reviews

--- cleaning/views.py
+++ cleaning/views.py
@@ -6,13 +6,15 @@ from django.db import IntegrityError
 from .forms import ReviewForm
 from .models import Review
 
+
 def signupUser(request):
     if request.method == 'GET':
         return render(request, 'signupUser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
                 user.save()
                 print(user)
                 login(request, user)
@@ -23,7 +25,7 @@ def signupUser(request):
         else:
             print("passwords din't match")
             return render(request, 'signupUser.html', {'form': UserCreationForm(), 'errors': "your passwords did not match"})
-            
+
 
 def loginUser(request):
     if request.method == 'GET':
@@ -31,12 +33,14 @@ def loginUser(request):
         return render(request, 'login.html', {'form': AuthenticationForm()})
     else:
         errors = ""
-        user = authenticate(request, username=request.POST["username"], password=request.POST["password"])
+        user = authenticate(
+            request, username=request.POST["username"], password=request.POST["password"])
         if user is None:
             return render(request, "login.html", {"form": AuthenticationForm(), "errors": "username or password doesn't exists"})
         else:
             login(request, user)
             return redirect('/')
+
 
 def logoutUser(request):
     if request.method == 'POST':
@@ -47,17 +51,38 @@ def logoutUser(request):
         return render(request, 'home.html')
         print("didn't logout")
 
+
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        totalReviews = reviews.__len__()
+        averageReviews = 0
+        for review in reviews:
+            averageReviews += review.rating
+            formatedReviews = []
+        averageReviews /= totalReviews
+        averageReviews = int(averageReviews)
+        formatedAverageReviews = []
+        for average in range(averageReviews):
+            formatedAverageReviews.append("")
+
+        return render(request, 'home.html', {
+            "totalReviews": totalReviews,
+            "averageReviews": formatedAverageReviews
+        })
+
 
 def services(request):
     return render(request, 'services.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def services(request):
     return render(request, 'services.html')
+
 
 def reviews(request):
     if request.method == 'GET':
@@ -66,21 +91,43 @@ def reviews(request):
         averageReviews = 0
         for review in reviews:
             averageReviews += review.rating
-            formatedR = []
+            formatedReviews = []
             for rating in range(review.rating):
-                formatedR.append("")
-                review.rating = formatedR
-        print(reviews)
-
+                formatedReviews.append("")
+                review.rating = formatedReviews
         averageReviews /= totalReviews
         averageReviews = int(averageReviews)
+        formatedAverageReviews = []
+        for average in range(averageReviews):
+            formatedAverageReviews.append("")
 
         return render(request, 'reviews.html', {
-            "reviews": reviews,
             "totalReviews": totalReviews,
-            "averageReviews": averageReviews
+            "averageReviews": formatedAverageReviews
         })
-    
+
+    # if request.method == 'GET':
+    #     reviews = Review.objects.all()
+    #     totalReviews = reviews.__len__()
+    #     averageReviews = 0
+    #     for review in reviews:
+    #         averageReviews += review.rating
+    #         formatedR = []
+    #         for rating in range(review.rating):
+    #             formatedR.append("")
+    #             review.rating = formatedR
+    #     print(reviews)
+
+    #     averageReviews /= totalReviews
+    #     averageReviews = int(averageReviews)
+
+    #     return render(request, 'reviews.html', {
+    #         "reviews": reviews,
+    #         "totalReviews": totalReviews,
+    #         "averageReviews": averageReviews
+    #     })
+
+
 def addReview(request):
     if request.method == 'GET':
         return render(request, 'addReview.html')
@@ -100,5 +147,3 @@ def addReview(request):
                 return redirect('/reviews')
             except ValueError:
                 return render(request, 'addReview.html', {'errors': "you entered in bad information"})
-
-        
